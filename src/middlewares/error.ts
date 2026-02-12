463d4861d6208ab6f03f4b340bb40fffaa1c322d
Anujahirwar01 <anujahirwar77010@gmail.com>
2026-02-13 00:51:30 +0530

    Fix error middleware to return correct status codes

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -6,11 +6,13 @@ import ErrorHandler from '../utils/utility-class.js';
 export const errorMiddleware = (err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
     err.message = err.message || 'Internal Server Error';
     err.statusCode = err.statusCode || 500;
+    
     if(err.name === 'CastError'){
         err.message = "Invalid ID";
         err.statusCode = 400;
     }
-    return res.status(400).json({
+    
+    return res.status(err.statusCode).json({
         success: false,
         message: err.message
     })
